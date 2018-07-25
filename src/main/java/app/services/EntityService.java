7b30606e2f14aa045aeb1356156e2dcc412dfe90
hexymouse <hexymouse@gmail.com>
2018-07-25 09:15:16 +0300

    interface

--- src/main/java/app/services/EntityService.java
+++ src/main/java/app/services/EntityService.java
@@ -3,7 +3,7 @@ package app.services;
 import app.dto.EntityDto;
 
 public interface EntityService {
-    public void createEntity(EntityDto entityDto);
+    void createEntity(EntityDto entityDto);
 
-    public String getAllEntities();
+    String getAllEntities();
 }
