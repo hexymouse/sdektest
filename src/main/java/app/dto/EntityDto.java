99015318c79961ce63008fe444b7bb813c4afd76
hexymouse <hexymouse@gmail.com>
2018-07-25 08:36:30 +0300

    dto

---
+++ src/main/java/app/dto/EntityDto.java
@@ -0,0 +1,25 @@
+package app.dto;
+
+public class EntityDto {
+
+    private Long id;
+    private String name;
+
+    public EntityDto() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
