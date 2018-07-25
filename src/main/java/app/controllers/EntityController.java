6da0c24bdc1b0b3b7d732f1eb5faa802e5a0885c
hexymouse <hexymouse@gmail.com>
2018-07-25 08:19:29 +0300

    entities controller

--- src/main/java/app/controllers/EntityController.java
+++ src/main/java/app/controllers/EntityController.java
@@ -4,15 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/entity")
 public class EntityController {
 
-    @GetMapping("/entity")
+    @GetMapping
     public String getEntities() {
         return "list of entities";
     }
 
-    @PostMapping("/entity")
+    @PostMapping
     public String createEntity() {
         return "create entity";
     }
